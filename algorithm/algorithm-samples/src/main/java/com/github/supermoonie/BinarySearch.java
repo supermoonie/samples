4b221235ad573fe3ba4b2ff83441970ace428d47
supermoonie <wangchao_moonie@163.com>
2020-03-19 09:41:49 +0800

    add algorithm-samples

---
+++ algorithm/algorithm-samples/src/main/java/com/github/supermoonie/BinarySearch.java
@@ -0,0 +1,32 @@
+package com.github.supermoonie;
+
+/**
+ * @author supermoonie
+ * @date 2020-03-19
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] list = new int[]{1, 3, 5, 7, 9};
+        int index = BinarySearch.search(list, 5);
+        System.out.println(index);
+    }
+
+    private static int search(int[] list, int key) {
+        int low = 0;
+        int high = list.length;
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int ele = list[mid];
+            if (key == ele) {
+                return mid;
+            }
+            if (ele > key) {
+                low ++;
+            } else {
+                high --;
+            }
+        }
+        return -1;
+    }
+}
