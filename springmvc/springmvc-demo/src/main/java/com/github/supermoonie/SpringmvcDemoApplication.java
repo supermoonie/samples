10a0ccc490fb54b90b23247930e7a52fd7d51243
supermoonie <wangchao_moonie@163.com>
2020-04-04 13:06:36 +0800

    add springmvc-demo

---
+++ springmvc/springmvc-demo/src/main/java/com/github/supermoonie/SpringmvcDemoApplication.java
@@ -0,0 +1,16 @@
+package com.github.supermoonie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author supermoonie
+ * @date 2020-04-04
+ */
+@SpringBootApplication
+public class SpringmvcDemoApplication {
+
+    public static void main(String[] args) {
+        new SpringApplication().run(args);
+    }
+}
