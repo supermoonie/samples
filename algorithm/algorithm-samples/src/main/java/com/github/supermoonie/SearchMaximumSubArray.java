da2c95ac93ce34ae4fd6ea0721be90873c642a0e
supermoonie <wangchao_moonie@163.com>
2020-04-13 07:40:30 +0800

    add SearchMaximumSubArray

--- algorithm/algorithm-samples/src/main/java/com/github/supermoonie/SearchMaximumSubArray.java
+++ algorithm/algorithm-samples/src/main/java/com/github/supermoonie/SearchMaximumSubArray.java
@@ -1,5 +1,8 @@
 package com.github.supermoonie;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * 最大子数组查找
  *
@@ -9,20 +12,46 @@ package com.github.supermoonie;
 public class SearchMaximumSubArray {
 
     public static void main(String[] args) {
+        Random random = new Random();
+        int[] randoms = random.ints(10, -50, 50).distinct().toArray();
+        System.out.println(Arrays.toString(randoms));
+        System.out.println(searchMaxSubArrayByTwoFor(randoms));
+    }
 
+    /**
+     * 暴力解法，两层循环遍历
+     *
+     * @param array 数组
+     * @return 最大子数组
+     */
+    private static MaximumSubArray searchMaxSubArrayByTwoFor(int[] array) {
+        int max = array[0], left = 0, right = 0;
+        for (int i = 0; i < array.length; i++) {
+            int sum = 0;
+            for (int j = i; j < array.length; j++) {
+                sum = sum + array[j];
+                if (sum > max) {
+                    max = sum;
+                    left = i;
+                    right = j;
+                }
+            }
+        }
+        return new MaximumSubArray(left, right, max);
     }
 
     private static MaximumSubArray searchMaxCrossingSubArray(int[] array, int low, int mid, int high) {
         int leftSum = array[low];
         int sum = 0;
         int maxLeftIndex = -1;
-        for (int i = mid; i >= low; i --) {
+        for (int i = mid; i >= low; i--) {
             sum = sum + array[i];
             if (sum > leftSum) {
                 leftSum = sum;
                 maxLeftIndex = i;
             }
         }
+        return null;
     }
 
     static class MaximumSubArray {
@@ -43,6 +72,15 @@ public class SearchMaximumSubArray {
             this.leftIndex = leftIndex;
             this.rightIndex = rightIndex;
             this.sum = sum;
+        }
+
+        @Override
+        public String toString() {
+            return "MaximumSubArray{" +
+                    "leftIndex=" + leftIndex +
+                    ", rightIndex=" + rightIndex +
+                    ", sum=" + sum +
+                    '}';
         }
     }
 }
