7d608bb3772ca0df131cf11de2434253fca073fb
wang_chao <chao.wang@guanaitong.com>
2020-08-21 10:06:40 +0800

    javafx package

---
+++ javafx/javafx-samples/src/main/java/com/github/supermoonie/AppLauncher.java
@@ -0,0 +1,14 @@
+package com.github.supermoonie;
+
+import javafx.application.Application;
+
+/**
+ * @author supermoonie
+ * @since 2020/8/21
+ */
+public class AppLauncher {
+
+    public static void main(String[] args) {
+        Application.launch(App.class);
+    }
+}
